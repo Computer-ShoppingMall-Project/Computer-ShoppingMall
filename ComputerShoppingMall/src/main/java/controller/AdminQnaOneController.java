5ee80fd82633c1ad6c95921a9f5b64525639c23f
박범진 <@DESKTOP-LDR8D7J>
2022-05-19 13:12:19 +0900

    [박범진] Admin QnA 오류 수정

--- ComputerShoppingMall/src/main/java/controller/AdminQnaOneController.java
+++ ComputerShoppingMall/src/main/java/controller/AdminQnaOneController.java
@@ -1,13 +1,13 @@
 package controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.QnaDao;
 import vo.Qna;
--- ComputerShoppingMall/src/main/java/controller/AdminUpdateQnaController.java
+++ ComputerShoppingMall/src/main/java/controller/AdminUpdateQnaController.java
@@ -1,11 +1,13 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.QnaDao;
 
